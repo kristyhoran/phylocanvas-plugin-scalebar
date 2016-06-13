a7d288c5907ff5be471147519c1e327245f25c68
KAD <kad0xf@gmail.com>
2016-06-13 13:17:39 +0100

    Add options.

--- README.md
+++ README.md
@@ -13,6 +13,32 @@ Phylocanvas.plugin(scalebarPlugin);
 
 Phylocanvas.createTree('id', {
   // config defaults
-  scalebar: {}
+  scalebar: {
+    width: 200,
+    height: 20,
+    lineWidth: 2,
+    strokeStyle: 'black',
+    fillStyle: 'black',
+    font: '16px Sans-serif',
+    digits: 2,
+    position: {
+      bottom: 0,
+      centre: 0,
+    },
+  }
 })
 ```
+
+## Options
+
+A list of available options:
+* `width`: The width of the scale-bar in pixels. The default is `200` pixels.
+* `height`: The height of the scale-bar in pixels. The default is `20` pixels.
+* `lineWidth`: The thickness of scale-bar lines in space units. The default is `2`.
+* `strokeStyle`: Specifies the colour or style to use for scale-bar lines. The default is `#000` (black).
+* `fillStyle`: Specifies the colour or style to use for scale-bar label. The default is `#000` (black).
+* `font`: Specifies the text style used when drawing scale-bar label. The default is `16px Sans-serif`.
+* `digits`: The number of digits to appear after the decimal point; this may be a value between 0 and 20, inclusive. The default is `2`.
+* `position`: Specifies the scale-bar position on the canvas (the default value is `{ bottom: 0, centre: 0, }`), it includes two position components (vertical and horizontal):
+  * supported vertical position values are `top`, `middle`, or `bottom`',
+  * supported horizontal position values are `left`, `centre`, or `right`'.
--- src/index.js
+++ src/index.js
@@ -1,46 +1,93 @@
-import { Tree } from 'phylocanvas';
+import { Tree, utils } from 'phylocanvas';
 
-const SCALEBAR_WIDTH = 300;
-const SCALEBAR_HEIGHT = 20;
+const { getPixelRatio } = utils.canvas;
+
+const DEFAULTS = {
+  width: 200,
+  height: 20,
+  fillStyle: 'black',
+  strokeStyle: 'black',
+  lineWidth: 2,
+  font: '16px Sans-serif',
+  textBaseline: 'top',
+  textAlign: 'center',
+  digits: 2,
+  position: {
+    bottom: 0,
+    centre: 0,
+  },
+};
+
+const INVALID_HORIZONTAL_POSITION = 'Invalid horizontal position specified' +
+  'Supported values are `left`, `centre`, or `right`';
+const INVALID_VERTICAL_POSITION = 'Invalid vertical position specified' +
+  'Supported values are `top`, `middle`, or `bottom`';
 
 function drawScalebar() {
-  const width = SCALEBAR_WIDTH / this.zoom;
-  const height = SCALEBAR_HEIGHT / this.zoom;
-  const centre = (this.canvas.canvas.width / 2 - this.offsetx * 2) / this.zoom;
-  const bottom = (this.canvas.canvas.height - this.offsety * 2) / this.zoom;
-  const x = centre - width / 2;
-  const y = bottom - height * 2;
+  const { scalebar, offsetx, offsety, zoom, branchScalar } = this;
+  const { width, height, position } = scalebar;
+  const cxt = this.canvas;
+  const canvas = cxt.canvas;
+  const pixelRatio = getPixelRatio(cxt);
 
-  this.canvas.clearRect(x, y, width, height * 2);
+  cxt.save();
+  cxt.scale(1 / zoom, 1 / zoom);
+  cxt.translate(-offsetx * pixelRatio, -offsety * pixelRatio);
 
-  this.canvas.font = `${Math.max(height, 1)}px Sans-serif`;
-  this.canvas.fillStyle = 'black';
-  this.canvas.strokeStyle = 'black';
-  this.canvas.textBaseline = 'top';
+  let x = 0;
+  if (typeof position.left !== 'undefined') {
+    x = scalebar.lineWidth + position.left;
+  } else if (typeof position.centre !== 'undefined') {
+    x = (canvas.width / 2) - (width / 2) + position.centre;
+  } else if (typeof position.right !== 'undefined') {
+    x = canvas.width - width - scalebar.lineWidth - position.right;
+  } else {
+    this.loadError(INVALID_HORIZONTAL_POSITION);
+  }
+  let y = 0;
+  if (typeof position.top !== 'undefined') {
+    y = position.top;
+  } else if (typeof position.middle !== 'undefined') {
+    y = (canvas.height / 2) - height + position.middle;
+  } else if (typeof position.bottom !== 'undefined') {
+    y = canvas.height - (height * 2) - position.bottom;
+  } else {
+    this.loadError(INVALID_VERTICAL_POSITION);
+  }
+  cxt.clearRect(x, y, width, height * 2);
 
-  this.canvas.beginPath();
-  this.canvas.moveTo(x, y + height / 2);
-  this.canvas.lineTo(x + width, y + height / 2);
-  this.canvas.stroke();
-  this.canvas.moveTo(x, y);
-  this.canvas.lineTo(x, y + height);
-  this.canvas.stroke();
-  this.canvas.moveTo(x + width, y);
-  this.canvas.lineTo(x + width, y + height);
-  this.canvas.stroke();
-  this.canvas.closePath();
+  cxt.font = scalebar.font;
+  cxt.fillStyle = scalebar.fillStyle;
+  cxt.strokeStyle = scalebar.strokeStyle;
+  cxt.lineWidth = scalebar.lineWidth;
+  cxt.textBaseline = scalebar.textBaseline;
+  cxt.textAlign = scalebar.textAlign;
 
-  const startLabel = '0';
-  const endLabel = (SCALEBAR_WIDTH / this.branchScalar / this.zoom).toFixed(6);
-  const startLabelSize = this.canvas.measureText(startLabel);
-  const endLabelSize = this.canvas.measureText(endLabel);
+  cxt.beginPath();
+  cxt.moveTo(x, y + height / 2);
+  cxt.lineTo(x + width, y + height / 2);
+  cxt.stroke();
+  cxt.moveTo(x, y);
+  cxt.lineTo(x, y + height);
+  cxt.stroke();
+  cxt.moveTo(x + width, y);
+  cxt.lineTo(x + width, y + height);
+  cxt.stroke();
+  cxt.closePath();
 
-  this.canvas.fillText(startLabel, x - startLabelSize.width / 2, y + height);
-  this.canvas.fillText(endLabel,
-    x + width - endLabelSize.width + startLabelSize.width / 2, y + height);
+  const label = (width / branchScalar / zoom).toFixed(scalebar.digits);
+  cxt.fillText(label, x + width / 2, y + height);
+
+  cxt.restore();
 }
 
-export default function historyPlugin(decorate) {
+export default function plugin(decorate) {
+  decorate(this, 'createTree', (delegate, args) => {
+    const tree = delegate(...args);
+    const [ , config = {} ] = args;
+    tree.scalebar = Object.assign({}, DEFAULTS, config.scalebar || {});
+    return tree;
+  });
   decorate(Tree, 'draw', function (delegate, args) {
     delegate.apply(this, args);
     drawScalebar.apply(this);
